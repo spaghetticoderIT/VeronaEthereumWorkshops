c14146b2c49294908872c485fd960599efa0c2bb
Jonbi2 <marcellobardus8@gmail.com>
2018-11-13 19:10:03 +0100

    ERC20

--- SmartContract/HelloWorldContract/contracts/HelloWorld.sol
+++ SmartContract/HelloWorldContract/contracts/HelloWorld.sol
@@ -5,32 +5,3 @@ contract HelloWorld {
         return "Hello World";
     }
 }
-
-pragma solidity ^0.4.24;
-
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/StandardToken.sol";
-import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/DetailedERC20.sol";
-
-contract VeronaEthereumMeetupToken is DetailedERC20, StandardToken {
-    constructor(string _name, string _symbol, uint8 _decimals, uint256 _amount) 
-    public DetailedERC20(_name, _symbol, _decimals) {
-        require(_amount > 0, "amount has to be greater than 0");
-        totalSupply_ = _amount.mul(10 ** uint256(_decimals));
-        balances[msg.sender] = totalSupply_;
-        emit Transfer(address(0), msg.sender, totalSupply_);
-    }   
-}
-
-var VeronaEthereumMeetupToken = artifacts.require(
-  "./VeronaEthereumMeetupToken.sol"
-);
-
-module.exports = function(deployer) {
-  deployer.deploy(
-    VeronaEthereumMeetupToken,
-    "VeronaEthereumMeetupToken",
-    "VMT",
-    18,
-    21000000
-  );
-};
