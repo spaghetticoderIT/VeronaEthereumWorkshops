1812c628e6de790646e7463edea31ccc60927f96
Jonbi2 <marcellobardus8@gmail.com>
2018-11-18 00:42:22 +0100

    Whisper done, TODO resolve the symKey issue

--- Web3Introduction/examples/whisper.js
+++ Web3Introduction/examples/whisper.js
@@ -1,25 +1,38 @@
 const Web3 = require("web3");
 const web3 = new Web3();
+
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
-web3.shh
-  .post({
-    symKeyID:
-      "321f3531fad3de1dd8344da26cba1a074ee3689acb796ecb8e5bdf8507b1a0ef", //symKeyID of the sender
+async function getKeyPair() {
+  const keyPair = await web3.shh.newKeyPair();
+  return keyPair;
+}
+
+async function postMessage(recipientPublicKey, topic, message) {
+  const messageHash = await web3.shh.post({
+    pubKey: recipientPublicKey,
     ttl: 10,
-    topic: "0x07678231",
-    powTarget: 2.01,
-    powTime: 2,
-    payload: web3.utils.fromAscii("Hello there!")
-  })
-  .then(function() {
-    web3.ssh
-      .newMessageFilter({
-        privateKeyID: receiver,
-        topics: ["0x07678231"]
-      })
-      .get_new_entries()
-      .then(function(message) {
-        console.log(message);
-      });
+    topic: topic,
+    powTarget: 0.5,
+    powTime: 3,
+    payload: web3.utils.fromAscii(message)
   });
+  return messageHash;
+}
+
+async function receiveMessage(keyPair) {
+  const filter = await web3.shh.newMessageFilter({
+    privateKeyID: keyPair
+  });
+  let messages = await web3.shh.getFilterMessages(filter);
+  return messages;
+}
+
+async function main() {
+  const keyPair = await getKeyPair();
+  const publicKey = await web3.shh.getPublicKey(keyPair);
+  await postMessage(publicKey, "0x07678231", "Hello World").then(console.log);
+  await receiveMessage(keyPair).then(console.log);
+}
+
+main();
