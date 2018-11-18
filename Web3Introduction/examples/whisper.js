5bb0dba63bab427a9a3b00ec0a874173f79c7fe9
marcello <marcello@marcellos-iMac.local>
2018-11-18 12:30:39 +0100

    The filter is configured properly, probably the problem is ttl

--- Web3Introduction/examples/whisper.js
+++ Web3Introduction/examples/whisper.js
@@ -4,14 +4,17 @@ const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider("http://localhost:8545"));
 
 async function getKeys() {
-  const symKeyID = await web3.shh.newKeyPair();
-  return { symKeyID };
+  const sig = await web3.shh.newKeyPair();
+  const privateKeyID = sig;
+  const pubKey = await web3.shh.getPublicKey(privateKeyID);
+  return { sig, privateKeyID, pubKey };
 }
 
-async function postMessage(recipientPublicKey, topic, message) {
+async function postMessage(recipientPubKey, sig, topic, message) {
   const messageHash = await web3.shh.post({
-    pubKey: recipientPublicKey,
-    ttl: 10,
+    pubKey: recipientPubKey,
+    sig: sig,
+    ttl: 15,
     topic: topic,
     powTarget: 0.5,
     powTime: 3,
@@ -30,9 +33,10 @@ async function receiveMessage(keyPair) {
 
 async function main() {
   const keys = await getKeys();
-  const publicKey = await web3.shh.getPublicKey(keys.symKeyID);
-  await postMessage(publicKey, "0x07678231", "Hello World").then(console.log);
-  await receiveMessage(keys.symKeyID).then(console.log);
+  await postMessage(keys.pubKey, keys.sig, "0x07678231", "Hello World").then(
+    console.log
+  );
+  await receiveMessage(keys.privateKeyID).then(console.log);
 }
 
 main();
