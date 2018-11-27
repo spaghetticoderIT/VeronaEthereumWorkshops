43ce4c690e9734e867d27c0131082199f723702d
Jonbi2 <marcellobardus8@gmail.com>
2018-11-27 11:01:09 +0100

    presentation update

--- Web3Introduction/examples/money.js
+++ Web3Introduction/examples/money.js
@@ -1,1 +1,13 @@
 const Web3 = require("web3");
+
+const web3 = new Web3();
+
+web3.setProvider(new web3.providers.HttpProvider("http://localhost:7545"));
+
+web3.eth.getAccounts().then(accounts => {
+  web3.eth.sendTransaction({
+    from: accounts[0],
+    to: accounts[1],
+    value: web3.utils.toWei("0.05", "ether")
+  });
+});
